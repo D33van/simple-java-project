6e9a4552a953e54289508263e93b56046bacad1a
D33van <42265368+D33van@users.noreply.github.com>
2024-09-02 12:48:40 +0530

    Update test.java

--- src/main/java/works/buddy/samples/test.java
+++ src/main/java/works/buddy/samples/test.java
@@ -1,18 +1,25 @@
-import java.util.zip.ZipFile;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
-⋮
-public void extract(ZipFile zip) {
-     ⋮
-     String toDir = "/my/target/directory/";
-     Enumeration entries = zip.entries();
-     while (entries.hasMoreElements()) {
+import java.util.zip.ZipFile;
+
+public void extract(ZipFile zipFile) {
+    String toDir = "/my/target/directory/";
+    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+    while (entries.hasMoreElements()) {
         ZipEntry zipEntry = entries.nextElement();
-        ⋮
-        File file = new File(toDir, zipEntry.getName())
-        InputStream istr = zipFile.getInputStream(zipEntry);
-        final OutputStream os = Files.newOutputStream(file.toPath());
-        bos  = new BufferedOutputStream(os);
-        IOUtils.copy(bis, bos);
-	
+        File file = new File(toDir, zipEntry.getName());
+        try (InputStream istr = zipFile.getInputStream(zipEntry);
+             OutputStream os = Files.newOutputStream(file.toPath())) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = istr.read(buffer)) > 0) {
+                os.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            // Handle exception
+        }
     }
 }
